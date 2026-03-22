77e286d78bd21ae1f352d9dfd3b699128bcc0b89
Eldar Aslanbeily <eldar.aslanbeily@gmail.com>
2026-03-22 14:38:29 +0200

    docs: restore rabbitmq yaml sample

--- DummyAppTests/Program.cs
+++ DummyAppTests/Program.cs
@@ -1,6 +1,1 @@
-using System.Reflection;
-
-Directory.SetCurrentDirectory(AppContext.BaseDirectory);
-Assembly.Load("QaaS.Common.Assertions");
-Assembly.Load("QaaS.Common.Generators");
 QaaS.Runner.Bootstrap.New(args).Run();
