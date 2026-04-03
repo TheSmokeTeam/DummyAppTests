9143cc5cdf2d85da457f53e0d469da5ef481c9f0
Eldar Aslanbeily <eldar.aslanbeily@gmail.com>
2026-04-03 17:13:17 +0300

    fix: set required metadata in smoke app

--- DummyAppTests/Program.cs
+++ DummyAppTests/Program.cs
@@ -1,5 +1,12 @@
+using QaaS.Framework.SDK;
+using QaaS.Framework.SDK.Extensions;
 using QaaS.Runner;
 
 var bootstrapArguments = args.Length > 0 ? args : ["run", "test.qaas.yaml"];
 var runner = Bootstrap.New(bootstrapArguments);
+runner.ExecutionBuilders.AsSingle().WithMetadata(new MetaDataConfig
+{
+    Team = "Smoke",
+    System = "DummyApp"
+});
 runner.Run();
